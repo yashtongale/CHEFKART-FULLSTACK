1200a57a98f07e23ea68b9cf2bec9c6fc71550a8
yashtongale <yashtongle298@gmail.com>
2026-01-31 00:15:26 +0530

    Update api.js

--- chefkart-frontend/src/services/api.js
+++ chefkart-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api/v1',
+    baseURL: 'https://chefkart-fullstack-03.onrender.com/api/v1',
     withCredentials: true
 });
 
